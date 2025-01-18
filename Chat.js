4138d68ca98d47e80d07f6ac07289b07e05af232
Myst Start <143287227+JRStudios88373@users.noreply.github.com>
2025-01-17 22:54:16 -0400

    Delete Chat.js
    Signed-off-by: Myst Start <143287227+JRStudios88373@users.noreply.github.com>

--- Chat.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const ChatSchema = new Schema({
-  nick: String,
-  msg: String,
-  created: { type: Date, default: Date.now },
-});
-
-export default model("Chat", ChatSchema);
